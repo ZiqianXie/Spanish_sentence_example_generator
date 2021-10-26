9e8a80a680344cf6f855876dad7616ee35745eaa
ZiqianXie <z.xie4@umiami.edu>
2021-10-25 22:12:45 -0500

    modify the element finding method

--- google_translate_crawl.py
+++ google_translate_crawl.py
@@ -10,20 +10,27 @@ browser = webdriver.Firefox(executable_path='geckodriver.exe')
 def get_example(word):
     query = 'https://translate.google.com/?sl=es&tl=en&text={}&op=translate'
     browser.get(query.format(word))
-    WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "VK4HE")))
-    time.sleep(0.5)
     try:
-        expand = browser.find_elements_by_class_name("VK4HE")[-1]
-        expand.click()
+        WebDriverWait(browser, 5).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "AZPoqf")))
+    except:
+        return []
+    time.sleep(0.1)
+    try:
+        for element in browser.find_elements_by_class_name("Dwvecf"):
+            if element.text.startswith('Examples'):
+                expand = element.find_element(By.XPATH, "following-sibling::*")
+                time.sleep(0.4)
+                expand.find_element_by_class_name('VK4HE').click()
+                break
     except:
         pass
-    l = list(map(lambda x: x.text, browser.find_elements_by_class_name("AZPoqf")))
+    m = list(map(lambda x: x.text, browser.find_elements_by_class_name("AZPoqf")))
     r = []
-    for oracion in l:
+    for oracion in m:
         browser.get(query.format(oracion))
         time.sleep(0.5)
-        translation = WebDriverWait(browser, 10).until(
+        translation = WebDriverWait(browser, 5).until(
             EC.presence_of_element_located((By.CLASS_NAME, "J0lOec")))
         r.append((oracion, translation.text.strip()))
     return r
